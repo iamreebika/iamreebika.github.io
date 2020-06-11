c90293722cf2e11da3d3c8c7b5d7165d601991f3
Reebika Bhatta <66770073+iamreebika@users.noreply.github.com>
2020-06-11 20:33:20 +0545

    Update func.js

--- func.js
+++ func.js
@@ -1,9 +1,7 @@
-console.log("This file is working");
-$.getJSON("https://api.github.com/users/iampujan/repos?per_page=5", function(json) {
+$.getJSON("https://api.github.com/users/iamreebika/repos?per_page=5", function(json) {
     var resp = '';
     $.each(json, function(index, value) {
       resp=resp+index + ' ' + value['html_url']+ ' -';
-      console.log(resp);
       $('#gitfunc').html(resp);
     });
   });
