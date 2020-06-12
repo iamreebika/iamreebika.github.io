006d64c9d4bec70953d51aea609d249b5523fe89
Reebika Bhatta <66770073+iamreebika@users.noreply.github.com>
2020-06-12 08:47:03 +0545

    Update func.js

--- func.js
+++ func.js
@@ -1,7 +1,25 @@
-$.getJSON("https://api.github.com/users/iamreebika/repos?per_page=5", function(json) {
-    var resp = '';
-    $.each(json, function(index, value) {
-      resp=resp+index + ' ' + value['html_url']+ ' -';
-      $('#gitfunc').html(resp);
+  let container = $('#pagination');
+      container.pagination({
+        pageSize	:5,
+      dataSource:  function(done) {
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.github.com/users/iamreebika/repos',
+            success: function(response) {
+                done(response);
+            }
+        });
+     },
+      callback: function (data, pagination) {
+          var dataHtml = '<ul>';
+  
+          $.each(data, function (index, item) {
+          dataHtml += '<li><a href="' + item.html_url + '">'+item.name+'</a></li>';
+          });
+  
+          dataHtml += '</ul>';
+          console.log(dataHtml);
+  
+          $("#repoContainer").html(dataHtml);
+      }
     });
-  });
